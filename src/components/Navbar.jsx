b83aba73c86585313c798d84e5579b2b458f1ccd
jebitok-dev <sharonkosgei4@gmail.com>
2022-10-08 23:59:28 +0300

    Add Changes to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,19 @@ const Navbar = () => {
   return (
     <div>
       <nav className='w-full flex py-6 justify-between items-center navbar'>
-        
+        <img src={logo} alt="hoobank" className='w-[124px] h-[32px' />
+        <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+            {navLinks.map((nav, index) => (
+              <li 
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white mr-10`}
+              >
+                <a href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+        </ul>
       </nav>
     </div>
   )
