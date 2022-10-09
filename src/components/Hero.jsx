805c5c8536347b7d5d2192e59a9f8cb2445acd66
jebitok-dev <sharonkosgei4@gmail.com>
2022-10-10 00:14:24 +0300

    Add Changes to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import styles from "../style";
 import { discount, robot } from "../assets";
-import {GetStarted} from "../components";
+import { GetStarted } from "../components";
 
 const Hero = () => {
 	return (
@@ -18,17 +18,33 @@ const Hero = () => {
 						<span className='text-white'>1 Month</span> Account
 					</p>
 				</div>
-        <div className="flex flex-row justify-between w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">The Next <br className="sm:block hidden"/> {" "} 
-          <span className="text-gradient">Generation
-          </span> {" "}
-          Payment Method.</h1>
-          <div className="ss:flex hidden md:mr-4 mr-0">
-            <GetStarted />
-          </div>
-        </div>
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">Payment Method</h1>
+				<div className='flex flex-row justify-between w-full'>
+					<h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
+						The Next <br className='sm:block hidden' />{" "}
+						<span className='text-gradient'>Generation</span>{" "}
+					</h1>
+					<div className='ss:flex hidden md:mr-4 mr-0'>
+						<GetStarted />
+					</div>
+				</div>
+				<h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
+					Payment Method
+				</h1>
+				<p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+					With the right credit card, you can improve your financial life by
+					building credit, earning rewards and saving money. But with hundreds
+					of credit cards on the market.
+				</p>
 			</div>
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"/>
+        <div className="absolute z-[1] w-[80%] h-[80%] bottom-40 white__gradient rounded-full"/>
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 top-20 blue__gradient"/>
+      </div>
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
 		</section>
 	);
 };
