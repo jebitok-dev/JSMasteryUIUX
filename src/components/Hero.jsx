f9079d531026892343e76fdaf54ed273881105c4
jebitok-dev <sharonkosgei4@gmail.com>
2022-10-09 23:59:57 +0300

    Add Changes to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,6 +27,7 @@ const Hero = () => {
             <GetStarted />
           </div>
         </div>
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">Payment Method</h1>
 			</div>
 		</section>
 	);
