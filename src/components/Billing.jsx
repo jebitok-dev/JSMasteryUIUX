317ededa37a75d29567b59dae1a74b6e38fbd33e
jebitok-dev <sharonkosgei4@gmail.com>
2022-10-10 23:42:44 +0300

    Add Changes to Billing

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,14 +1,32 @@
-import { apple, google, bill } from "../assets"
-import styles, {layout} from "../style";
+import { apple, google, bill } from "../assets";
+import styles, { layout } from "../style";
 
 const Billing = () => (
-  <section id="product" className={layout.sectionReverse}>
-    <div className={layout.sectionImgReverse}>
-      <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
-    </div>
-    <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounder-full white__gradient" />
-    <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounder-full pink__gradient" />
-  </section>
-)
+	<section id='product' className={layout.sectionReverse}>
+		<div className={layout.sectionImgReverse}>
+			<img
+				src={bill}
+				alt='billing'
+				className='w-[100%] h-[100%] relative z-[5]'
+			/>
+			<div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounder-full white__gradient' />
+			<div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounder-full pink__gradient' />
+		</div>
+		<div className={layout.sectionInfo}>
+			<h2 className={styles.heading2}>
+				Easily control your <br className='sm:block hidden' /> billing &
+				invoicing.
+			</h2>
+			<p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+				Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
+				neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+			</p>
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img src={apple} alt="apple_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+        <img src={apple} alt="google_play" className="w-[128px] h-[42px] object-contain cursor-pointer" />
+      </div>
+		</div>
+	</section>
+);
 
-export default Billing
+export default Billing;
