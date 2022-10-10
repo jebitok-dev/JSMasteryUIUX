225fe558714120b57e0f9bede1c49e00e9b7e6fc
jebitok-dev <sharonkosgei4@gmail.com>
2022-10-10 23:14:32 +0300

    Add Changes to Billing

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { apple, google, bill } from "../assets"
+import styles, {layout} from "../style";
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+    </div>
+    <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounder-full white__gradient" />
+    <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounder-full pink__gradient" />
+  </section>
+)
 
 export default Billing
