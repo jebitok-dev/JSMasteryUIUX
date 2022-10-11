bc17874f979f7a7d85d365bf2208948c7f2344eb
jebitok-dev <sharonkosgei4@gmail.com>
2022-10-11 23:27:40 +0300

    Add Changes to CarDeal section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -16,6 +16,9 @@ const CardDeal = () => (
 			</p>
       <Button styles="mt-10" />
 		</div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
 	</section>
 );
 
