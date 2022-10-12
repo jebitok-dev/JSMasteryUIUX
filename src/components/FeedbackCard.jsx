6c61ab6d30135bf43968d5467119ffaae2910130
jebitok-dev <sharonkosgei4@gmail.com>
2022-10-12 23:58:08 +0300

    Add Changes FeedbackCard

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -5,6 +5,13 @@ const FeedbackCard = ({content, name, title, img}) => (
     <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
       <img src={quotes} alt="double_quotes" className='w-[42px] h-[27px] object-contain' />
       <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'>{content}</p>
+      <div className='flex flex-row'>
+        <img src={img} alt="name" className='w-[48px] h-[48px] rounded-full' />
+        <div className='flex flex-col ml-4'>
+          <h4>{name}</h4>
+          <h4>{title}</h4>
+        </div>
+      </div>
     </div>
   )
 
