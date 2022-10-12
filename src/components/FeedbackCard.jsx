af598945cdb3477ac51f08b995878736f1aa866f
jebitok-dev <sharonkosgei4@gmail.com>
2022-10-12 23:54:41 +0300

    Add Changes Testimonial

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { quotes } from '../assets'
 
-const FeedbackCard = () => {
-  return (
-    <div>FeedbackCard</div>
+const FeedbackCard = ({content, name, title, img}) => (
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+      <img src={quotes} alt="double_quotes" className='w-[42px] h-[27px] object-contain' />
+      <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'>{content}</p>
+    </div>
   )
-}
 
 export default FeedbackCard
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -19,7 +19,11 @@ const Testimonials = () => (
 				</p>
 			</div>
 		</div>
-    <div className="flex flex-wrap sm:justify-start justify-center "
+    <div className="flex flex-wrap sm:justify-start justify-center feedback-container relative z-[1]">
+      {feedback.map ((card) => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
+    </div>
 	</section>
 );
 
