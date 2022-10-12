97a3e651b26003bd858663438e27a88c2bd9bd38
jebitok-dev <sharonkosgei4@gmail.com>
2022-10-13 00:03:52 +0300

    Add Changes FeedbackCard

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,7 +7,7 @@ const Testimonials = () => (
 		id='clients'
 		className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
 	>
-		<div />
+		<div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
 		<div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
 			<h1 className={styles.heading2}>
 				What people are <br className='sm:block hidden' /> saying about us
