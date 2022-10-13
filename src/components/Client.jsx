c4b126e7746e1ec7450f2b5e0aa689ac91309949
jebitok-dev <sharonkosgei4@gmail.com>
2022-10-13 23:55:42 +0300

    Add Changes Footer  Section

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -6,7 +6,7 @@ const Client = () => {
     <section className={`${styles.flexCenter} my-4`}>
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map((client) => (
-          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] hover:bg-white`}>
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] hover:bg-white cursor-pointer`}>
             <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain" />
           </div>
         ))}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,18 +21,32 @@ const Footer = () => (
 						key={footerLink.key}
 						className='flex flex-col ss:my-0 my-4 min-w-[150px]'
 					>
-						<h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">{footerLink.title}</h4>
-						<ul className="list-none mt-4">
+						<h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white'>
+							{footerLink.title}
+						</h4>
+						<ul className='list-none mt-4'>
 							{footerLink.links.map((link, index) => (
 								<li
 									key={link.name}
-									className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length - 1 ? 'mb-4' : 'mb-0'}`}
+									className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+										index !== footerLink.links.length - 1 ? "mb-4" : "mb-0"
+									}`}
 								>
 									{link.name}
 								</li>
 							))}
 						</ul>
 					</div>
+				))}
+			</div>
+		</div>
+		<div className="w-full flex justify-between items-center md:flex-row pt-6 border-t-[1px] border-t-[#3F3E45]">
+			<p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
+				2021 HooBank. All Rights Reserved.
+			</p>
+			<div className='flex flex-row md:mt-0 mt-6'>
+				{socialMedia.map((social, index) => (
+					<img key={social.id} src={social.icon} alt='social.id' className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? "mr-6" : "mr-0"}`} />
 				))}
 			</div>
 		</div>
